83d515f0460c38027988d91cd6c1bd47cc4637a5
Alberto Morcillo Sanz <amorcillosanz@gmail.com>
2025-10-22 19:04:04 +0200

    feat: multiple plots

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,11 @@ void candlestickWindow();
 void geometricBrownianMotionWindow();
 void indicatorsWindow();
 
+static float xs1[1001], ys1[1001];
+static float xs2[1001], ys2[1001];
+static float xs3[1001], ys3[1001];
+static float xs4[1001], ys4[1001];
+
 int main() {
 
     // Main window
@@ -59,26 +64,50 @@ void generalWindow() {
 
     ImGui::Separator();
 
-    static float xs1[1001], ys1[1001];
-    for (int i = 0; i < 1001; ++i) {
-        xs1[i] = i * 0.001f;
-        ys1[i] = 0.5f + 0.5f * sinf(50 * (xs1[i] + (float)ImGui::GetTime() / 10));
-    }
+    ImVec2 size = ImGui::GetContentRegionAvail();
+    //size.y = 3 * size.y / 4;
 
-    static double xs2[20], ys2[20];
-    for (int i = 0; i < 20; ++i) {
-        xs2[i] = i * 1/19.0f;
-        ys2[i] = xs2[i] * xs2[i];
-    }
+    if (ImPlot::BeginSubplots("NVDA", 1, 2, size)) {
 
-    if (ImPlot::BeginPlot("Line Plots")) {
-        ImPlot::SetupAxes("x","y");
-        ImPlot::PlotLine("f(x)", xs1, ys1, 1001);
-        ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
-        ImPlot::PlotLine("g(x)", xs2, ys2, 20,ImPlotLineFlags_Segments);
-        ImPlot::EndPlot();
-    }
+        for (int i = 0; i < 1001; ++i) {
+            xs1[i] = i * 0.01f;
+            ys1[i] = xs1[i] * sin(xs1[i]);
 
+            xs2[i] = i * 0.01f;
+            ys2[i] = xs2[i] * cos(xs2[i]);
+
+            xs3[i] = i * 0.01f;
+            ys3[i] = -xs2[i] * sin(xs2[i]);
+
+            xs4[i] = i * 0.01f;
+            ys4[i] = xs2[i] * tanh(xs3[i]);
+        }
+    
+        // Open
+        if (ImPlot::BeginPlot("Price")) {
+            
+            ImPlot::PlotLine("Open", xs1, ys1, 1001);
+            ImPlot::PlotLine("High", xs2, ys2, 1001);
+            ImPlot::PlotLine("Low", xs3, ys3, 1001);
+            ImPlot::PlotLine("Close", xs4, ys4, 1001);
+
+            ImPlot::EndPlot();
+        }
+
+        // Volume
+        if (ImPlot::BeginPlot("Volume")) {
+            static double xs4[100], ys4[100];
+            for (int i = 0; i < 100; i += 2) {
+                xs4[i] = i;
+                ys4[i] = fabs(sin(xs4[i] * 0.1));
+            }
+            ImPlot::PlotBars("sin(x)", xs4, ys4, 100, 2);
+            ImPlot::EndPlot();
+        }
+
+        ImPlot::EndSubplots();
+    }
+    
     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
     ImGui::End();
 }
